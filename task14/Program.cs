060936952e47f8b499c1b168808e737fbb908441
ulaev <ulaev1990@mail.ru>
2023-08-04 22:05:44 +0300

    Seminar 4.08.23

---
+++ task14/Program.cs
@@ -0,0 +1,20 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+bool MultiplicityTwoNumbers(int num, int num1, int num2)
+{
+    return num % num1 == 0 && num % num2 == 0;
+
+}
+
+bool result = MultiplicityTwoNumbers(number, 3, 21);
+
+Console.WriteLine(result ? "Кратно" : "Не кратно");
---
+++ task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Введите два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool checkSquareTwoNumbers(int firstNumber, int secondNumber)
+{
+    return firstNumber * firstNumber == secondNumber || secondNumber * secondNumber == firstNumber;
+
+}
+
+bool result = checkSquareTwoNumbers(num1, num2);
+Console.WriteLine(result? "Да": "Нет");
